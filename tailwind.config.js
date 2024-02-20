a6e2c2049e9baa97a409285e3244f7ed4dcff818
Erick Rodriguez <tbogard@outlook.com>
2024-02-19 21:18:42 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+const daisyui = require('daisyui')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: ['light']
+  }
+};
